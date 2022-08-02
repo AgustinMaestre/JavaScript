8562a7b895ff2101deb2efa67530a28a426a85e1
AgustinMaestre <agu_maestre@hotmail.com>
2022-08-02 17:23:26 -0300

    Desafio Complementario

---
+++ app.js
@@ -0,0 +1,14 @@
+/* let numMultiplicar = Number (prompt("Ingresa el numero de la tabla de multiplicar"));
+
+for (let i =1; i <= 10; i++){
+    let resultado = numMultiplicar * i;
+    alert(numMultiplicar + " * " + i + " = " + resultado)
+}
+ */
+
+/* let apellido = true; */
+
+do {
+    apellido = prompt("Ingresá tu apellido para, para finalizar escribí FIN")
+    console.log(apellido)
+} while (apellido != "FIN");
